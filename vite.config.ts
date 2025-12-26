bb129fa5955bb2fce5fc3a6d48293062838d179b
damianof <damianof@users.noreply.github.com>
2025-12-26 10:29:56 -0500

    Enhancements

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
 /// <reference types="vite/client" />
-
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts' // Add this
 import path from 'path'
 import { name } from './package.json'
 const projectName = name.replace('@tenorlab/', '').trim().toLowerCase()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [
+    dts({ 
+      rollupTypes: true, // This flattens Core's types into one file
+      insertTypesEntry: true 
+    })
+  ],
   envDir: './',
   resolve: {
     alias: {
